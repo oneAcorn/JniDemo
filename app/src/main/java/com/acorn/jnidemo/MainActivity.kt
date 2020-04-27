3d1cd7011fa4b312bddaae4323fb981528481943
Acorn <oneacorn@163.com>
2020-04-27 14:42:14 +0800

    test

--- app/src/main/java/com/acorn/jnidemo/MainActivity.kt
+++ app/src/main/java/com/acorn/jnidemo/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val helper=NativeHelper()
         // Example of a call to a native method
         sample_text.text = "${stringFromJNI()}\n测试2:${helper.testNdk()}"
